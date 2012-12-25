2b6edff31687b1cba756db8dd24a6f6e445226c2
Justin Mackenzie <jmackenzie1908@gmail.com>
2012-12-25 01:31:05 -0500

    Updated Attributes Class
    Added spacing for read-ability and removed the numerical definitions of
    all the fields.

--- Implementation/CharacterState/Attributes.cs
+++ Implementation/CharacterState/Attributes.cs
@@ -3,25 +3,35 @@ using System;
 public class Attributes
 {
 	/* Max energy. */
-	public float maxEnergy = 100.0f;
+	public float maxEnergy;
+	
 	/* Speed while walking. */
-	public float walkingSpeed = 6.0f;
+	public float walkingSpeed;
+	
 	/* Speed while sprinting. */
-	public float sprintingSpeed = 9.0f;
+	public float sprintingSpeed;
+	
 	/* Speed while crouching. */
-	public float crouchingSpeed = 3.0f;
+	public float crouchingSpeed;
+	
 	/* Current speed. */
 	public float currentSpeed;
+	
 	/* Energy cost of sprinting for 1 second. */
-	public float sprintCost = 20.0f;
+	public float sprintCost;
+	
 	/* Energy to be regenerated after 1 second of idle time. */
-	public float idleEnergyRegenRate = 20.0f;
+	public float idleEnergyRegenRate;
+	
 	/* Energy to be regenerated after 1 second of moving time. */
-	public float movingEnergyRegenRate = 15.0f;
+	public float movingEnergyRegenRate;
+	
 	/* The current amount of energy. */
 	public float currentEnergy;
+	
 	/* The point at which entity is no longer fatigued. */
-	public float fatigueThreshold = 20.0f;
+	public float fatigueThreshold;
+	
 	/* Determines if the entity is fatigued. */
 	public bool fatigued;
 	
