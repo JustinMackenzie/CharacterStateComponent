be0bd4807ae4d3ebbc0d16e54f0363289b38fcf4
Justin Mackenzie <jmackenzie1908@gmail.com>
2012-12-25 19:46:03 -0500

    Updated Source Code
    Enclosed all classes in a namespace "CharacterState".

--- Implementation/CharacterState/Sprinting.cs
+++ Implementation/CharacterState/Sprinting.cs
@@ -1,62 +1,65 @@
-using System;
+﻿using System;
 
-public class Sprinting: MovementState
+namespace CharacterState
 {
-	#region Constructors
-	
-	/* Default constructor. */
-	public Sprinting ()
-	{
+    class Sprinting : MovementState
+    {
 
-	}
-	
-	#endregion
-	
-	#region Movement State Methods
-	
-	/* A move is performed. */
-	public void Move(CharacterState context)
-	{
-		return;
-	}
-	
-	/* Movement is stopped. */
-	public void Stop(CharacterState context)
-	{
-		context.SetMovementState(new StandingMoving());
-	}
-	
-	/* A crouch is performed. */
-	public void Crouch(CharacterState context)
-	{
-		context.SetMovementState(new CrouchingMoving());
-	}
-	
-	/* A jump is performed. */
-	public void Jump(CharacterState context)
-	{
-		context.SetMovementState(new Jumping());
-	}
-	
-	/* A sprint is performed. */
-	public void Sprint(CharacterState context)
-	{
-		return;
-	}
-	
-	/* The current state is complete. */
-	public void Complete(CharacterState context)
-	{
-		return;
-	}
-	
-	/* Returns the current state. */
-	public string State()
-	{
-		return GetType().ToString();
-	}
-	
-	#endregion
-	
+        #region Constructors
+
+        /* Default constructor. */
+        public Sprinting()
+        {
+
+        }
+
+        #endregion
+
+        #region Movement State Methods
+
+        /* A move is performed. */
+        public void Move(CharacterState context)
+        {
+            return;
+        }
+
+        /* Movement is stopped. */
+        public void Stop(CharacterState context)
+        {
+            context.SetMovementState(new StandingMoving());
+        }
+
+        /* A crouch is performed. */
+        public void Crouch(CharacterState context)
+        {
+            context.SetMovementState(new CrouchingMoving());
+        }
+
+        /* A jump is performed. */
+        public void Jump(CharacterState context)
+        {
+            context.SetMovementState(new Jumping());
+        }
+
+        /* A sprint is performed. */
+        public void Sprint(CharacterState context)
+        {
+            return;
+        }
+
+        /* The current state is complete. */
+        public void Complete(CharacterState context)
+        {
+            return;
+        }
+
+        /* Returns the current state. */
+        public string State()
+        {
+            return GetType().ToString();
+        }
+
+        #endregion
+
+    }
 }
-
